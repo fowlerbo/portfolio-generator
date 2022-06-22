1cd7406cffb2c5e3753c7e474c07262ffeb60b8a
bryant fowler <fowler1221bryant@gmail.com>
2022-06-22 14:20:17 -0400

    9.1.7

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
 const animalArray = ["dog", "cat", "pig"];
 
 animalArray.push("cow");
@@ -10,8 +13,19 @@ const personObj = {
 personObj.age = 100;
 personObj.occupation = "Developer";
 
+//Notice the lack of parentheses around the ` parameter?
 const printProfileData = (profileDataArr) => {
-  console.log(profileDataArr);
+  // This...
+  for (let i = 0; i < profileDataArr.length; i += 1) {
+    console.log(profileDataArr[i]);
+  }
+
+  console.log("================");
+
+  // Is the same as this...
+  profileDataArr.forEach((profileItem) => {
+    console.log(profileItem);
+  });
 };
 
 printProfileData(profileDataArgs);
